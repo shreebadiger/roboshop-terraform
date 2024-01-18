5e5355e1b5039919c17fb3b5340050635de245d1
shrinivas <shinivassb16@gmail.com>
2024-01-19 02:10:54 +0530

    updated

--- vars.tf
+++ vars.tf
@@ -1,12 +1,12 @@
- variable "env"{}
- variable "tags"{}
- variable "default_vpc_cidr"{}
- variable "default_vpc_id"{}
- variable "default_route_table_id"{}
- variable "account_id"{}
- variable "vpc"{}
- variable "route53_zone_id"{}
-variable "kms"{}
-variable "certificate_arn"{}
-variable "rds"{}
+    variable "env"{}
+    variable "tags"{}
+    variable "default_vpc_cidr"{}
+    variable "default_vpc_id"{}
+    variable "default_route_table_id"{}
+    variable "account_id"{}
+    variable "vpc"{}
+    variable "route53_zone_id"{}
+    variable "kms"{}
+    variable "certificate_arn"{}
+    variable "rds"{}
 
